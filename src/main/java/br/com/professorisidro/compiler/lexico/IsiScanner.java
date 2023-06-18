1ee5db9b8f3cdbd6a8c0eea396eae2e18eb20cd6
lucas_lsgsma <lucas.guimaraes@qintess.com>
2023-06-18 17:38:00 -0300

    initial commit

---
+++ src/main/java/br/com/professorisidro/compiler/lexico/IsiScanner.java
@@ -0,0 +1,128 @@
+package br.com.professorisidro.compiler.lexico;
+
+import br.com.professorisidro.compiler.exceptions.IsiLexicalException;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class IsiScanner {
+
+    private char[] content;
+    private int estado;
+    private int pos;
+
+    public IsiScanner(String filename) {
+        try {
+            String txtConteudo;
+            txtConteudo = new String(Files.readAllBytes(Paths.get(filename)), StandardCharsets.UTF_8);
+            System.out.println("DEBUG --------");
+            System.out.println(txtConteudo);
+            content = txtConteudo.toCharArray();
+            pos = 0;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public Token nextToken() {
+        char currentChar;
+        Token token = null;
+        String term = "";
+
+        estado = 0;
+        currentChar = nextChar();
+        while (!isEOF()) {
+            switch (estado) {
+                case 0 -> {
+                    if (isChar(currentChar)) {
+                        term += currentChar;
+                        estado = 1;
+                    } else if (isDigit(currentChar)) {
+                        term += currentChar;
+                        estado = 3;
+                    } else if (isSpace(currentChar)) {
+                        estado = 0;
+                    } else if (isOperator(currentChar)) {
+                        estado = 5;
+                    } else {
+                        throw new IsiLexicalException("Unrecognized SYMBOL");
+                    }
+                }
+                case 1 -> {
+                    if (isChar(currentChar) || isDigit(currentChar)) {
+                        estado = 1;
+                        term += currentChar;
+                    } else if (isSpace(currentChar) || isOperator(currentChar)) {
+                        estado = 2;
+                    } else {
+                        throw new IsiLexicalException("Malformed Identifier");
+                    }
+                }
+                case 2 -> {
+                    back();
+                    token = new Token();
+                    token.setType(Token.TK_IDENTIFIER);
+                    token.setText(term);
+                    return token;
+                }
+                case 3 -> {
+                    if (isDigit(currentChar)) {
+                        estado = 3;
+                        term += currentChar;
+                    } else if (!isChar(currentChar)) {
+                        estado = 4;
+                    } else {
+                        throw new IsiLexicalException("Unrecognized NUMBER");
+                    }
+                }
+                case 4 -> {
+                    back();
+                    token = new Token();
+                    token.setType(Token.TK_NUMBER);
+                    token.setText(term);
+                    return token;
+                }
+                case 5 -> {
+                    back();
+                    term += currentChar;
+                    token = new Token();
+                    token.setType(Token.TK_OPERATOR);
+                    token.setText(term);
+                    return  token;
+                }
+                default -> {}
+            }
+            currentChar = nextChar();
+        }
+        return null;
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    private boolean isChar(char c) {
+        return (c >= 'a' && c <= 'z') || (c>= 'A' && c <= 'Z');
+    }
+
+    private boolean isOperator(char c) {
+        return c == '>' || c == '<' || c == '=' || c == '!';
+    }
+
+    private boolean isSpace(char c) {
+        return c == ' ' || c == '\t' || c == '\n' || c == '\r';
+    }
+
+    private char nextChar() {
+        return content[pos++];
+    }
+
+    private boolean isEOF() {
+        return pos == content.length;
+    }
+
+    private void back() {
+        pos--;
+    }
+}
